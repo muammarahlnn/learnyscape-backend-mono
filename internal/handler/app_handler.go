99368ef133dbdf32aa560ee96d7719c21d6c92f7
muammarahlnn <21muammarahlan@gmail.com>
2025-04-25 17:07:44 +0800

    feat: handle http route not found and method not allowed

--- internal/handler/app_handler.go
+++ internal/handler/app_handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"learnyscape-backend-mono/pkg/dto"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -14,7 +15,34 @@ func NewAppHandler() *AppHandler {
 }
 
 func (h *AppHandler) Route(r *gin.Engine) {
-	r.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, "gokss air verse is cooking here!")
-	})
+	r.NoRoute(h.routeNotFound)
+	r.NoMethod(h.methodNotAllowed)
+	r.GET("/ping", h.ping)
+}
+
+func (h *AppHandler) ping(ctx *gin.Context) {
+	ctx.JSON(
+		http.StatusOK,
+		dto.WebResponse[any]{
+			Message: "pong",
+		},
+	)
+}
+
+func (h *AppHandler) routeNotFound(ctx *gin.Context) {
+	ctx.JSON(
+		http.StatusOK,
+		dto.WebResponse[any]{
+			Message: "route not found",
+		},
+	)
+}
+
+func (h *AppHandler) methodNotAllowed(ctx *gin.Context) {
+	ctx.JSON(
+		http.StatusOK,
+		dto.WebResponse[any]{
+			Message: "method not allowed",
+		},
+	)
 }
