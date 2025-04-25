508eb0dac8ce8936f3eb8877f699284adc92b607
muammarahlnn <21muammarahlan@gmail.com>
2025-04-25 16:42:33 +0800

    feat: add logger middleware

---
+++ pkg/middleware/logger_middleware.go
@@ -0,0 +1,50 @@
+package middleware
+
+import (
+	"learnyscape-backend-mono/pkg/logger"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+func LoggerMiddleware(logger logger.Logger) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		start := time.Now()
+		path := ctx.Request.URL.Path
+		ctx.Next()
+
+		params := map[string]any{
+			"status_code": ctx.Writer.Status(),
+			"client_ip":   ctx.ClientIP(),
+			"method":      ctx.Request.Method,
+			"latency":     time.Since(start).String(),
+			"path":        path,
+		}
+
+		if len(ctx.Errors) == 0 {
+			logger.WithFields(params).Info("incoming request")
+			return
+		}
+
+		logErrors(ctx, logger, params)
+	}
+}
+
+func logErrors(ctx *gin.Context, logger logger.Logger, params map[string]any) {
+	errors := []error{}
+	for _, err := range ctx.Errors {
+		switch err.Err.(type) {
+		case *validator.ValidationErrors:
+			params["status_code"] = http.StatusBadRequest
+			errors = append(errors, err)
+		default:
+			params["status_code"] = http.StatusInternalServerError
+			errors = append(errors, err)
+		}
+	}
+
+	params["errors"] = errors
+	logger.WithFields(params).Error("got errors")
+}
