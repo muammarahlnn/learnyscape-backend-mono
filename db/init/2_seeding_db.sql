6f43ddafeb4635923191cf84d98bafacb7bba649
muammarahlnn <21muammarahlan@gmail.com>
2025-05-07 23:26:24 +0800

    fix: fix seeding roles

--- db/init/2_seeding_db.sql
+++ db/init/2_seeding_db.sql
@@ -1,8 +1,8 @@
 INSERT INTO roles (name)
 VALUES
-    ('admin'),
-    ('lecturer'),
-    ('student');
+    ('Admin'),
+    ('Lecturer'),
+    ('Student');
 
 INSERT INTO users (role_id, username, email, hash_password, full_name)
 VALUES (1, 'admin', 'admin@unhas.ac.id', 'Admin123', 'Admin 1');
